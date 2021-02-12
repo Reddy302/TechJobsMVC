2c4263e7475a6c7e8212ce8041637f805803e057
Reddy Lewis <lewismd3283@gmail.com>
2021-02-11 23:51:46 -0600

    I think I'm done!

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsMVC.Data;
+using TechJobsMVC.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,6 +19,25 @@ namespace TechJobsMVC.Controllers
             return View();
         }
 
-        // TODO #3: Create an action method to process a search request and render the updated search view. 
+        // TODO #3: Create an action method to process a search request and render the updated search view.
+        [HttpGet]
+        [HttpPost]
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Job> jobs;
+            if (searchTerm == null)
+            {
+                jobs = JobData.FindAll();
+                ViewBag.title = "All Jobs";
+            }
+            else
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.title = $"SEARCH TERM: {searchTerm.ToLower()} CATEGORY SELECTED: {searchType.ToLower()}";
+                // Ask about the newline not working.
+            }
+            ViewBag.jobs = jobs;
+            return View();
+        }
     }
 }
